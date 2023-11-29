8eae29a411c8ee93a96f0d1922879df2f331f6e3
Scronkfink <145482124+Scronkfink@users.noreply.github.com>
2023-11-29 11:11:59 -0500

    Ports set by .env

---
+++ Notification-Service/server/notificationServer.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const PORT = process.env.BILLING_PORT
+require('dotenv').config()
+
+const mongoURI = process.env.MONGO_URI_USERS
+
+mongoose.connect(mongoURI);
+
+mongoose.connection.once('open', () => {
+    console.log('connected to the users DB by the users server')
+});
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+});
