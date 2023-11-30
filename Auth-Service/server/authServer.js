57f5d4f67072d11f7f045a65b1b9482160d15704
Scronkfink <145482124+Scronkfink@users.noreply.github.com>
2023-11-30 16:35:24 -0500

    Controllers Assembled

--- Auth-Service/server/authServer.js
+++ Auth-Service/server/authServer.js
@@ -15,6 +15,18 @@ mongoose.connection.once('open', () => {
 
 app.use(express.json());
 
+
+
+app.use((req, res, err, next) => {
+    const defaultError = {
+        log: 'There was an unknown middleware error in Authentication',
+        status: 500,
+        message: 'Housten, there\'s been an authentication problem',
+    };
+    const errObj = Object.assign(defaultError, err)
+    res.status(errObj.status).json(errObj.message)
+});
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
