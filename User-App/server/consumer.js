aa53e4c5a3aef4564a286b7ee901fce37c6b5fbb
Jake Gibson <jakesgibson@gmail.com>
2023-12-02 16:37:20 -0500

    Begin implementation of websocket for consumer.js

--- User-App/server/consumer.js
+++ User-App/server/consumer.js
@@ -1,4 +1,5 @@
 const amqp = require('amqplib');
+let globalWs;
 
 const exchangeName = 'trekker_topic';
 
@@ -14,11 +15,78 @@ export const receiveMsg = async () => {
   channel.consume(
     AppQueue,
     (msg) => {
-      const msgObj = JSON.parse(msg.content.toString());
-      console.log(`[x] App received: ${msgObj}`);
+      const msgObj = msg.content.toString();
+      console.log(
+        `[x] App received: ${msgObj}, now to get this to the frontend...`
+      );
+
+      //ws function
+      globalWs.send(msgObj); //send json back to fe via ws with instructions in body
     },
     {
       noAck: true,
     }
   );
 };
+
+/**
+ * Web Socket
+ *
+ * It's possible that we just call the publisher.js sendMsg here
+ * so that we don't have to go thru the server
+ */
+
+const { WebSocketServer } = require('ws');
+const wsserver = new WebSocketServer({ port: 443 });
+
+wsserver.on('connection', (ws) => {
+  globalWs = ws;
+  // ws.session = { secret: 'Secret Info Here' };
+  // ws.on('close', () => dbg('Client has disconnected!'));
+
+  ws.on('message', async (rawMessage) => {
+    // let message;
+    //   try {
+    //     message = JSON.parse(rawMessage);
+    //   } catch (err) {
+    //     return dbg('Could not parse message: ', rawMessage);
+    //   }
+    //   // TODO:
+    //   let response;
+    //   try {
+    //     switch (message.type) {
+    //       case '':
+    //         // NOTE: Initial handshake is done in Express. user_id must exist at this point.
+    //         console.log('Client joined.');
+    //         // Store properties on ws.session
+    //         ws.session.user_id = message.user_id;
+    //     }
+    //     const state = {
+    //           user_id: ws.session.user_id,
+    //           room,
+    //           entries,
+    //         };
+    //         const response = JSON.stringify({
+    //           type: 'init',
+    //           state,
+    //         });
+    //         ws.send(response);
+    //         // break;
+    //         } catch (err) {
+    //     console.log(
+    //       `ERROR: ${JSON.stringify(err)}. Unable to handle message: `,
+    //       message
+    //     );
+    //   }
+  });
+});
+
+//   fetch('/rabbit', {
+//     method: 'POST',
+//     headers: { 'Content-type': 'application/json' },
+//     body: msgObj,
+//   })
+//   .then(res => console.log('[x] Rabbit message sent to server'))
+//   .catch(err => {
+//     console.log(err)
+//   })
--- User-App/server/server.js
+++ User-App/server/server.js
@@ -18,19 +18,29 @@ app.use(express.json());
 app.use(cookieParser());
 
 //serves files for the webpack
-app.use('/build', express.static(path.join(__dirname, '../build')));
+app.use('/assets', express.static(path.join(__dirname, './client/assets')));
 
 //functional endpoints
-app.post('/login', async (req, res) => {
-  const routingKey = 'Auth';
-  const msg = req.body.messsage;
-  await sendMsg(routingKey, msg);
-  // await receiveMsg(); //?? is this the way to do it maybe make a websocket
-  res.send('');
+app.post('/auth', async (req, res) => {
+  await sendMsg('Auth', req.body.messsage); //message could be login or signup
+  res.send();
 });
 
+app.post('/inv', async (req, res) => {
+  await sendMsg('Inv', req.body.messsage); //message could be load or checkout
+  res.send();
+});
+
+//rabbitMQ endpoint (if ws isnt implemented in consumer.js)
+
+// app.use('/rabbit', (req, res) => {
+//   //ws function
+//   res.send()
+// })
+
 //used for serving the application
-app.use('/', (req, res) => {
+app.use('/', async (req, res) => {
+  await receiveMsg();
   res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
@@ -54,51 +64,4 @@ app.use((err, req, res, next) => {
 //connects the server to the port
 app.listen(3000, () => {
   console.log(`server listening on port ${PORT}`);
-});
-
-/**
- * Web Socket
- */
-
-const { WebSocketServer } = require('ws');
-const wsserver = new WebSocketServer({ port: 443 });
-
-wsserver.on('connection', (ws) => {
-  // ws.session = { secret: 'Secret Info Here' };
-  // ws.on('close', () => dbg('Client has disconnected!'));
-  // ws.session = { secret: 'Secret Info Here' };
-  // ws.on('close', () => dbg('Client has disconnected!'));
-  // let message;
-  //   try {
-  //     message = JSON.parse(rawMessage);
-  //   } catch (err) {
-  //     return dbg('Could not parse message: ', rawMessage);
-  //   }
-  //   // TODO:
-  //   let response;
-  //   try {
-  //     switch (message.type) {
-  //       case '':
-  //         // NOTE: Initial handshake is done in Express. user_id must exist at this point.
-  //         console.log('Client joined.');
-  //         // Store properties on ws.session
-  //         ws.session.user_id = message.user_id;
-  //     }
-  //     const state = {
-  //           user_id: ws.session.user_id,
-  //           room,
-  //           entries,
-  //         };
-  //         const response = JSON.stringify({
-  //           type: 'init',
-  //           state,
-  //         });
-  //         ws.send(response);
-  //         // break;
-  //         } catch (err) {
-  //     console.log(
-  //       `ERROR: ${JSON.stringify(err)}. Unable to handle message: `,
-  //       message
-  //     );
-  //   }
 });
