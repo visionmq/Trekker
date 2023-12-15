fc1386a4581ffba554899119e2900aa8a16ce240
Cheri French <clynnfrench@gmail.com>
2023-12-15 15:50:05 -0500

    Formatting switch/case

--- User-App/client/redux/reducers/websocketMiddleware.js
+++ User-App/client/redux/reducers/websocketMiddleware.js
@@ -1,6 +1,6 @@
 import { updateAllProperties, updateAvailableDates, updateLoadState } from "./propertySlice";
 import { checkoutOutcome } from "./checkoutSlice";
-import { signupCurrentUser, loginCurrentUser, failedLogin, failedSignup,  } from "./userSlice";
+import { signupCurrentUser, loginCurrentUser, failedLogin, failedSignup, } from "./userSlice";
 
 
 
@@ -8,43 +8,43 @@ import { signupCurrentUser, loginCurrentUser, failedLogin, failedSignup,  } from
 
 const websocketMiddleware = (wsUrl) => {
   let socket = null;
-  
+
   const onOpen = (store) => (event) => {
     /**Not Used */
   };
-  
+
   const onClose = (store) => (event) => {
     /**Not Used */
   };
 
   const onMessage = (store) => async (event) => {
-    let action 
+    let action
     try {
       action = await JSON.parse(event.data)
 
       switch (action.socketAction) {
-      case 'updateInventoryState':
-              store.dispatch(updateAllProperties({properties: action.properties, status: 'success'}));
+        case 'updateInventoryState':
+          store.dispatch(updateAllProperties({ properties: action.properties, status: 'success' }));
           break;
-  
+
         case 'propertySearchFailed':
-            store.dispatch(updateLoadState({status: 'failed'})) 
+          store.dispatch(updateLoadState({ status: 'failed' }))
           break;
-          
+
         case 'noAvail':
-          store.dispatch(updateAvailableDates({quantity: 0}))
+          store.dispatch(updateAvailableDates({ quantity: 0 }))
           break; //I might need to come back and change this based on how the components subscribe to state 
-        
-        case 'orderComplete': 
-        store.dispatch(checkoutOutcome({outcome: true}))
+
+        case 'orderComplete':
+          store.dispatch(checkoutOutcome({ outcome: true }))
           break;
 
         case 'billingFailed':
-          store.dispatch(checkoutOutcome({outcome: false}))
+          store.dispatch(checkoutOutcome({ outcome: false }))
           break;
 
         case 'signupSuccessful':
-          store.dispatch(signupCurrentUser({userInfo: action.user }));
+          store.dispatch(signupCurrentUser({ userInfo: action.user }));
           break;
 
         case 'signupFailed':
@@ -52,42 +52,42 @@ const websocketMiddleware = (wsUrl) => {
           break;
 
         case 'loginSuccessful':
-          store.dispatch(loginCurrentUser({userInfo: action.user }));
+          store.dispatch(loginCurrentUser({ userInfo: action.user }));
           break;
 
         case 'loginFailed':
           store.dispatch(failedLogin());
           break;
-          default:
-            return next(action);
-    }
+        default:
+          return next(action);
+      }
     }
     catch (err) {
       console.log('Parsing OR Switch case error. ')
     }
   };
-    return (store) => (next) => (action) => {
-      console.log('Websocket Action: ', action.socketAction)
-      const socketAction = action.socketAction;
-  
-      console.log('Dispatch Middleware has received a message. Here are the contents: ', action);
-      
-      switch (socketAction) {
-        case 'middlewareConnect':
+  return (store) => (next) => (action) => {
+    console.log('Websocket Action: ', action.socketAction)
+    const socketAction = action.socketAction;
 
-          if (socket === null) {
-            // Create a new WebSocket connection
-            socket = new WebSocket(wsUrl);
-            socket.onopen = onOpen(store);
-            socket.onclose = onClose(store);
-            socket.onmessage = onMessage(store);
-          }
-          break;
-          default:
-            return next(action);
-      }
-    };
+    console.log('Dispatch Middleware has received a message. Here are the contents: ', action);
+
+    switch (socketAction) {
+      case 'middlewareConnect':
+
+        if (socket === null) {
+          // Create a new WebSocket connection
+          socket = new WebSocket(wsUrl);
+          socket.onopen = onOpen(store);
+          socket.onclose = onClose(store);
+          socket.onmessage = onMessage(store);
+        }
+        break;
+      default:
+        return next(action);
+    }
   };
+};
 
 
 
